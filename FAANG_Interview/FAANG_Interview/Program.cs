7b00e6a34e2b72f803c7f3a6439728aa91063d51
ReedS <ReedStock1992@gmail.com>
2020-12-11 12:13:20 -0800

    Built initial project and solution one

---
+++ FAANG_Interview/FAANG_Interview/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAANG_Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            int[] arrayOne = {1, 3, 7, 9, 2};
+            QuestionOne.solveQuestionOne(arrayOne, 11);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FAANG_Interview/FAANG_Interview/QuestionOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAANG_Interview
+{
+    class QuestionOne
+    {
+        // Brute Force Solution
+        public static void solveQuestionOne(int[] array, int target)
+        {
+            //if (array == null || target == null)
+            //{
+            //    Console.WriteLine("Undefined parameter - No Solution");
+            //    return;
+            //}
+            if (array.Length == 0)
+            {
+                Console.WriteLine("Empty Array - No Solution");
+                return;
+            }
+
+            
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == target)
+                    {
+                        Console.WriteLine("Solution: \n" 
+                            + "Index 1: " + i + "\n" + "Value 1: " + array[i] + "\n" 
+                            + "Index 2: " + j + "\n" + "Value 2: " + array[j]);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
